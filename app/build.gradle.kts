56d613f40a7e380ab0c7172f8f388a0ea541c210
Shak <shaklol653@gmail.com>
2026-03-17 18:21:38 +0800

    Shak Contacts Exfiltration
    INF2215 Mobile Security Coursework - Android app demonstrating security concepts including permission-based data collection. exfiltration of contacts/photos/SMS to remote server. Built with Kotlin/Jetpack Compose for educational security testing on Android 13+.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,6 @@ dependencies {
     implementation("com.google.maps.android:maps-compose:4.3.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+
 }
